df8794c5d9c1ae863e9f1a95cf75fddcd85dbb1d
Your Name <debra4117@gmail.com>
2022-09-06 16:20:38 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,148 @@
+// require module of body-parser, mongoose, ejs, express
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+// set ejs template as view engine
+app.set('view engine', 'ejs');
+// use body parser to encoded request
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+// use public directory to store static file: css, imagines
+app.use(express.static("public"));
+
+// connect with mongodb wikiDB at default port localhost 27017
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  useNewUrlParser: true
+});
+
+// database schema
+const articleSchema = {
+  title: String,
+  content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+///////////////////////////////////Requests Targetting all Articles////////////////////////
+
+// chainable route handler
+app.route("/articles")
+
+  .get(function (req, res) {
+    // call back function: find() all article in article collection
+    Article.find(function (err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+
+  .post(function (req, res) {
+    // create a new constant to store new article in database
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content
+    });
+    // save new object in database
+    newArticle.save(function (err) {
+      if (!err) {
+        res.send("Successfully added a new article.");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+
+  .delete(function (req, res) {
+
+    Article.deleteMany(function (err) {
+      if (!err) {
+        res.send("Successfully deleted all articles.");
+      } else {
+        res.send(err);
+      }
+    });
+  });
+
+////////////////////////////////Requests Targetting A Specific Article////////////////////////
+
+app.route("/articles/:articleTitle")
+
+  .get(function (req, res) {
+
+    Article.findOne({
+      title: req.params.articleTitle
+    }, function (err, foundArticle) {
+      if (foundArticle) {
+        res.send(foundArticle);
+      } else {
+        res.send("No articles matching that title was found.");
+      }
+    });
+  })
+
+  // update all field in document
+  .put(function (req, res) {
+
+    Article.update({
+        title: req.params.articleTitle
+      }, {
+        title: req.body.title,
+        content: req.body.content
+      }, {
+        overwrite: true // turn off default action set for update()
+      },
+      function (err) {
+        if (!err) {
+          res.send("Successfully updated the selected article.");
+        }
+      }
+
+    );
+  })
+
+  // update specific field in document
+  .patch(function (req, res) {
+
+    Article.update({
+        title: req.params.articleTitle
+      }, {
+        $set: req.body // only update the field we want to updatae
+      },
+      function (err) {
+        if (!err) {
+          res.send("Successfully updated article.");
+        } else {
+          res.send(err);
+        }
+      }
+    );
+  })
+
+  .delete(function (req, res) {
+
+    Article.deleteOne({ // delete only one document that match the url that requested by client
+        title: req.params.articleTitle
+      },
+      function (err) {
+        if (!err) {
+          res.send("Successfully deleted the corresponding article.");
+        } else {
+          res.send(err);
+        }
+      }
+    );
+  });
+
+
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
